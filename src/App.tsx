fbd867d6596d1f3da775562807b98733898f5982
MwaniN <brooklynewyork@gmail.com>
2024-05-08 17:55:25 -0400

    Centered text

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      This is the portfolio page
+
+    <div className="flex h-screen">
+      <div className="m-auto">
+      <h1 className="text-3xl text-center font-bold">
+      Hello, my name is <span className="text-3xl text-red-500 font-bold">Mwani</span>.
     </h1>
+    <h1 className="text-3xl text-center font-bold">
+      I am a full stack software engineer.
+    </h1>
+      </div>
+    </div>
   )
 }
 export default App
