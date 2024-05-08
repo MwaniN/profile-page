2f9ef432c6179bd33978e51ea6c6b4b4d2facdb2
MwaniN <brooklynewyork@gmail.com>
2024-05-08 19:16:08 -0400

    Custom font, responsive lines

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
 
     <div className="flex h-screen">
       <div className="m-auto">
-      <h1 className="text-3xl text-center font-bold">
-      Hello, my name is <span className="text-3xl text-red-500 font-bold">Mwani</span>.
+      <h1 className="text-5xl text-center font-hindSilliguri font-thin leading-10">
+      Hello, I'm <span className="text-5xl text-red-500 font-hindSilliguri font-thin leading-10">Mwani</span>.
     </h1>
-    <h1 className="text-3xl text-center font-bold">
-      I am a full stack software engineer.
+    <h1 className="text-5xl text-center font-hindSilliguri font-thin leading-10">
+      I'm a full stack software engineer.
     </h1>
       </div>
     </div>
