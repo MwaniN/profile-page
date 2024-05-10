fc30e08d219310774941b11050c1b66b5ad4f667
MwaniN <brooklynewyork@gmail.com>
2024-05-10 00:17:39 -0400

    Leading and margin added

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
     <div className="bg-black absolute h-screen w-screen">
       <canvas id="canvas" className="h-screen w-screen absolute"></canvas>
     <div className="flex h-screen">
-      <div className="m-auto">
+      <div className="m-auto z-10">
       <h1 className="text-5xl text-center text-white font-hindSilliguri font-thin leading-10">
-      Hello, I'm <span className="text-5xl text-red-500 font-hindSilliguri font-thin leading-10">Mwani</span>.
+      Hello, I'm <span className="text-5xl text-red-500 font-hindSilliguri font-thin leading-10 inline-block">Mwani</span>.
     </h1>
-    <h1 className="text-5xl text-white text-center font-hindSilliguri font-thin leading-10">
+    <h1 className="text-5xl text-white text-center font-hindSilliguri font-thin leading-15 my-2">
       I'm a full stack software engineer.
     </h1>
       </div>
