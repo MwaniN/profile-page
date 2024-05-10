33bacbb2acbb952617aaa853ed6143c9674b81e7
MwaniN <brooklynewyork@gmail.com>
2024-05-09 21:37:07 -0400

    Style updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ function App() {
 
   return (
 
+    <div className="bg-black">
     <div className="flex h-screen">
       <div className="m-auto">
-      <h1 className="text-5xl text-center font-hindSilliguri font-thin leading-10">
+      <h1 className="text-5xl text-center text-white font-hindSilliguri font-thin leading-10">
       Hello, I'm <span className="text-5xl text-red-500 font-hindSilliguri font-thin leading-10">Mwani</span>.
     </h1>
-    <h1 className="text-5xl text-center font-hindSilliguri font-thin leading-10">
+    <h1 className="text-5xl text-white text-center font-hindSilliguri font-thin leading-10">
       I'm a full stack software engineer.
     </h1>
       </div>
+    </div>
     </div>
   )
 }
