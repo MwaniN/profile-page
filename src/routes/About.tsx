b74c4c940b7e5307d03e4dbeec98abf27853c718
MwaniN <brooklynewyork@gmail.com>
2024-05-17 17:38:34 -0400

    Cleanup

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
 const About = () => {
   return (
     <div id="about" className="bg-slate-500 min-h-screen min-w-screen">
-      <div className='text-4xl bg-orange-300 text-white'>About Me</div>
+      <div className='text-4xl bg-orange-300 text-white m-auto w-min'>About</div>
     </div>
   )
 }
