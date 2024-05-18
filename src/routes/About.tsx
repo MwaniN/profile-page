29a4e4fca5f26be6049bc37db355c8a91f0e89a0
MwaniN <brooklynewyork@gmail.com>
2024-05-18 16:59:33 -0400

    About section changed - icon setup

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,9 +1,12 @@
 // import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div id="about" className="bg-slate-500 min-h-screen min-w-screen">
+    <div className="bg-black min-h-screen min-w-screen">
+      <div id="about" className='min-h-screen min-w-screen'>
       <div className='text-4xl bg-orange-300 text-white m-auto w-min'>About</div>
+      </div>
     </div>
   )
 }
