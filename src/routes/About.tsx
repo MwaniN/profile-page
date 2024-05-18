66e29497b70768271f5702a4ed90fd5f87b1ec36
MwaniN <brooklynewyork@gmail.com>
2024-05-18 18:12:31 -0400

    Nav bar started and basic navigation working

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,20 +1,19 @@
 // import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-
-const bars = <FontAwesomeIcon icon={faBars} />
 
 const About = () => {
   return (
     <div className="bg-black min-h-screen min-w-screen">
       <div id="about" className='min-h-screen min-w-screen'>
-        <header className='bg-gray-800'>
-          <div>
-            <button>
-              {bars}
-            </button>
-          </div>
+        <header className='flex items-center bg-gray-800 justify-end px-3 py-3'>
+            <ul className='text-blue-200 flex items-center'>
+              <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+              <Link to="/#main">Home</Link>
+                </li>
+              <li className='px-1 hover:text-white focus:text-white focus:outline-none'>About</li>
+              <li className='px-1 hover:text-white focus:text-white focus:outline-none'>Projects</li>
+              <li className='px-1 hover:text-white focus:text-white focus:outline-none'>Contact</li>
+            </ul>
         </header>
       <div className='text-4xl bg-orange-300 text-white m-auto w-min'>About</div>
       </div>
