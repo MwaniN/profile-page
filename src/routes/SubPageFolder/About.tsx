f272ca9bf995717d5ff1a4fda8d91f876c9abea9
MwaniN <brooklynewyork@gmail.com>
2024-05-20 13:10:05 -0400

    Separated header

--- src/routes/SubPageFolder/About.tsx
+++ src/routes/SubPageFolder/About.tsx
@@ -1,25 +1,11 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Header from './Header/Header.tsx';
 
 const About = () => {
 
 
   return (<div id="about" className='min-h-screen min-w-screen'>
-  <header className='flex items-center bg-gray-800 justify-end px-3 py-3 sticky top-1'>
-      <ul className='text-blue-200 flex items-center'>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
-        <Link to="/#main">Home</Link>
-          </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
-        <Link to="/#about">About</Link>
-        </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
-        <Link to="/#projects">Projects</Link>
-        </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
-        <Link to="/#contact">Contact</Link>
-        </li>
-      </ul>
-  </header>
+    <Header />
 <div className='text-4xl bg-orange-300 text-white m-auto w-min'>About</div>
 </div>)
 }
---
+++ src/routes/SubPageFolder/Header/Header.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+
+  return (
+    <header className='flex items-center bg-gray-800 justify-end px-3 py-3 sticky top-1'>
+      <ul className='text-blue-200 flex items-center'>
+        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <Link to="/#main">Home</Link>
+          </li>
+        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <Link to="/#about">About</Link>
+        </li>
+        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <Link to="/#projects">Projects</Link>
+        </li>
+        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <Link to="/#contact">Contact</Link>
+        </li>
+      </ul>
+  </header>
+  )
+}
+
+export default Header;
