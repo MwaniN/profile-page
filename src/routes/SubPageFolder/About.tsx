375cd280af6e215be7e0ee1583b035ee22d43026
MwaniN <brooklynewyork@gmail.com>
2024-05-20 14:43:48 -0400

    About section started

--- src/routes/SubPageFolder/About.tsx
+++ src/routes/SubPageFolder/About.tsx
@@ -9,12 +9,19 @@ const About = () => {
   // create image asset folder for png files
   // deliver short story and show technologies.
   // div 1 > image div - text div < div 1
+  // use an svg path to create the image for above in css
   // div 2 > images all with their own div and a border <div 2
   // make them side by side on larger screens and on top of the other on smaller screens
 
   return (<div id="about" className='min-h-screen min-w-screen'>
     <Header />
-<div className='text-4xl bg-orange-300 text-white m-auto w-min font-hindSilliguri font-thin'>About</div>
+<div className='text-4xl md:text-5xl lg:text-6xl text-white m-auto w-min font-hindSilliguri font-thin mt-16'>About</div>
+<div className='text-base tracking-normal text-left text-wrap text-white font-hindSilliguri font-thin leading-relaxed mx-2 mt-5'>
+  I live to learn something new every day. I also enjoy making things.
+  I also enjoy figuring out solutions to things. I actually enjoy trying to untangle knots, because it's fun and can be a meditative practice where you constantly have to
+  check / reevaluate your assumptions. Software engineering is fun because you get to create and learn things at the same time! When not creating or learning,
+  I may be catching a movie, enjoying nature, or exercising.
+</div>
 </div>)
 }
 
