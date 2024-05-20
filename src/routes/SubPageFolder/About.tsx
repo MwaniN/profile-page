a9efa0083e38dd698441d5257c4fe1cc05829bbb
MwaniN <brooklynewyork@gmail.com>
2024-05-20 13:37:30 -0400

    Notes

--- src/routes/SubPageFolder/About.tsx
+++ src/routes/SubPageFolder/About.tsx
@@ -3,6 +3,14 @@ import Header from './Header/Header.tsx';
 
 const About = () => {
 
+  // start with two divs
+  // each div shall be flex.. so it can be responsive
+  // sub divs for text boxes and images
+  // create image asset folder for png files
+  // deliver short story and show technologies.
+  // div 1 > image div - text div < div 1
+  // div 2 > images all with their own div and a border <div 2
+  // make them side by side on larger screens and on top of the other on smaller screens
 
   return (<div id="about" className='min-h-screen min-w-screen'>
     <Header />
