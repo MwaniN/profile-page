0684bd59e79c28125aae5c3ff03574015052628f
MwaniN <brooklynewyork@gmail.com>
2024-05-20 13:30:07 -0400

    Responsive nav bar

--- src/routes/SubPageFolder/About.tsx
+++ src/routes/SubPageFolder/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
 
   return (<div id="about" className='min-h-screen min-w-screen'>
     <Header />
-<div className='text-4xl bg-orange-300 text-white m-auto w-min'>About</div>
+<div className='text-4xl bg-orange-300 text-white m-auto w-min font-hindSilliguri font-thin'>About</div>
 </div>)
 }
 
--- src/routes/SubPageFolder/Header/Header.tsx
+++ src/routes/SubPageFolder/Header/Header.tsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 
   return (
-    <header className='flex items-center bg-gray-800 justify-end px-3 py-3 sticky top-1'>
+    <header className='flex items-center bg-gray-800 justify-center px-3 py-3 sticky top-1 text-base md:text-xl md:justify-end font-hindSilliguri font-thin'>
       <ul className='text-blue-200 flex items-center'>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <li className='px-3 md:px-4 hover:text-white focus:text-white focus:outline-none'>
         <Link to="/#main">Home</Link>
           </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <li className='px-3 md:px-4 hover:text-white focus:text-white focus:outline-none'>
         <Link to="/#about">About</Link>
         </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <li className='px-3 md:px-4 hover:text-white focus:text-white focus:outline-none'>
         <Link to="/#projects">Projects</Link>
         </li>
-        <li className='px-1 hover:text-white focus:text-white focus:outline-none'>
+        <li className='px-3 md:px-4 md:mr-32 hover:text-white focus:text-white focus:outline-none'>
         <Link to="/#contact">Contact</Link>
         </li>
       </ul>
