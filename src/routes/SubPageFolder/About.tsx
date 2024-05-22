61c66ca6837ca44235addc822d1f70e5cef87270
MwaniN <brooklynewyork@gmail.com>
2024-05-22 15:30:09 -0400

    Adding svg and support

--- src/routes/SubPageFolder/About.tsx
+++ src/routes/SubPageFolder/About.tsx
@@ -1,5 +1,6 @@
 // import { Link } from 'react-router-dom';
 import Header from './Header/Header.tsx';
+import Person from "../assets/person-outline.svg";
 
 const About = () => {
 
@@ -16,6 +17,7 @@ const About = () => {
   return (<div id="about" className='min-h-screen min-w-screen'>
     <Header />
 <div className='text-4xl md:text-5xl lg:text-6xl text-white m-auto w-min font-hindSilliguri font-thin mt-16 md:mb-8'>About</div>
+<Person />
 <div className='text-base tracking-normal lg:w-1/3 md:m-auto text-left text-wrap text-white font-hindSilliguri font-thin leading-relaxed mx-2 sm:mt-8 px-3 py-2'>
   I live to learn something new every day. I also enjoy making things.
   I also enjoy figuring out solutions to things. I actually enjoy trying to untangle knots, because it's fun and can be a meditative practice where you constantly have to
