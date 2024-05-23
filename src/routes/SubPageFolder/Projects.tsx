0b4e66277aef767e6cb485a1d4137eb271249d0f
MwaniN <brooklynewyork@gmail.com>
2024-05-22 21:43:26 -0400

    Slight content changes

--- src/routes/SubPageFolder/Projects.tsx
+++ src/routes/SubPageFolder/Projects.tsx
@@ -3,7 +3,7 @@
 const Projects = () => {
   return (
     <div id="projects" className="min-h-screen min-w-screen">
-      <div className="text-green-300 m-auto">Projects</div>
+      <div className="text-green-300 m-auto">Project list coming soon</div>
     </div>
   )
 }
