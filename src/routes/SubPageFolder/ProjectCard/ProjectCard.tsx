ee4d83d08f808d61feb28195f85ef0c7c6364e41
MwaniN <brooklynewyork@gmail.com>
2024-05-24 19:51:39 -0400

    Notes

--- src/routes/SubPageFolder/ProjectCard/ProjectCard.tsx
+++ src/routes/SubPageFolder/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,6 @@
 // template / outline for each project represented.
 // pass in info from the top level componenet calling it
 // use grid to overlay pieces
+
+// include background image
+// name and link if applicable, also "Learn More" should go to GitHub
